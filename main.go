d06cc41a1a0de93e56191a769ce4045388379f01
zsong266 <student-00-e9d82d98bd6c@qwiklabs.net>
2026-03-22 03:32:16 +0000

    Deploy dev v2.0 with red handler

--- main.go
+++ main.go
@@ -26,9 +26,15 @@ import (
 
 func main() {
 	http.HandleFunc("/blue", blueHandler)
+	http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
-
+func redHandler(w http.ResponseWriter, r *http.Request) {
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
